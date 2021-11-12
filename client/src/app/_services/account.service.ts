1485b391a1872a51d31243fb145d97056c25c599
piotr_pik <piotr.pik@postdata.pl>
2021-11-12 03:52:13 +0100

    new changes

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = 'https://localhost:5001/api/';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+      map((resp: any) => {
+        const user = resp;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user))
+        }
+      })
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+  }
+}
